328accf7ea41cd20983f279393355a7b3091911f
RonitDhal <dhalronit@gmail.com>
2024-03-30 14:44:26 -0400

    initial commit

---
+++ src/main/java/com/cicd2/jenkinscicd/JenkinscicdApplication.java
@@ -0,0 +1,32 @@
+package com.cicd2.jenkinscicd;
+
+
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class JenkinscicdApplication {
+	static Logger logger = Logger.getLogger(String.valueOf(JenkinscicdApplication.class));
+
+	@PostConstruct
+	public  void init(){
+		logger.info("application started");
+	}
+
+	public static void main(String[] args) {
+
+
+		logger.info("application started");
+
+
+
+		SpringApplication.run(JenkinscicdApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/cicd2/jenkinscicd/JenkinscicdApplicationTests.java
@@ -0,0 +1,21 @@
+package com.cicd2.jenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class JenkinscicdApplicationTests {
+
+	static Logger logger = Logger.getLogger(String.valueOf(JenkinscicdApplicationTests.class));
+
+	@Test
+	void contextLoads() {
+		logger.info("test case executing");
+		assertEquals(true,true);
+	}
+
+}
